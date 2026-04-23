462eacbdae1813bab1d3694c91aa2623cc2c57d1
pablowells <pablobwells7@gmail.com>
2026-04-22 21:47:22 -0700

    Fix Bug 3

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add & typeof(num1) == typeof(num2) == Number) {
+    if (add && typeof(num1) == typeof(num2) == Number) {
         let result = 0;
 
         result = num1 + num2;
