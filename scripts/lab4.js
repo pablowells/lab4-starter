13cc1c235282c4ffa1c7e9ddb291b564e995b061
pablowells <pablobwells7@gmail.com>
2026-04-22 21:53:57 -0700

    Fix Bugs 5

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add && typeof(num1) == Number && typeof(num2) == Number) {
+    if (add === true && typeof num1 === 'number' && typeof num2 === 'number') {
         let result = 0;
 
         result = num1 + num2;
@@ -25,6 +25,13 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (
+        !Array.isArray(prices) ||
+        typeof discount !== 'number' ||
+        prices.length === 0
+    ) {
+        return false;
+    }
     const discounted = [];
     const length = prices.length;
     let discountedPrice = 0;
